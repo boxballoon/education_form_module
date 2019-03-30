b1544c3f9a28f3a01cbbfee71e35fe4ccd624ae5
Geras <coderholiq@gmail.com>
2019-03-30 03:34:19 -0400

    initial update

--- src/index.php
+++ src/index.php
@@ -14,29 +14,69 @@
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js" type="text/javascript"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/parsley.js/2.8.2/parsley.min.js" type="text/javascript"></script>
 
-    <form id="demo-form" action="" method="post">
-
+    <form id="demo-form" action="" method="post" autocomplete="off">
       <?php include "greenhouse/schools.php" ?>
-
+      <label for="name">School</label>
+      <input name="name" id="name" list="school-list" oninput="onInput()" required="">
       <input type="submit" name="submit" value="Submit">
-
+      <datalist id="school-list">
+      </datalist>
     </form>
 
 
     <script type="text/javascript">
-    $(function () {
-      $('#demo-form').parsley().on('field:validated', function() {
-        var ok = $('.parsley-error').length === 0;
-        $('.bs-callout-info').toggleClass('hidden', !ok);
-        $('.bs-callout-warning').toggleClass('hidden', ok);
-      })
-      .on('form:submit', function() {
+      var baseUrl = "https://boards-api.greenhouse.io/v1/boards/romeopower/education/schools";
+      var selectedSchoolId = "";
 
-        console.log("LOG THE VALUE OF THE SEARCH FIELD HERE.");
-
-        return false; // Don't submit form for this demo
+      $(function () {
+        $('#demo-form').parsley().on('field:validated', function() {
+          var ok = $('.parsley-error').length === 0;
+          $('.bs-callout-info').toggleClass('hidden', !ok);
+          $('.bs-callout-warning').toggleClass('hidden', ok);
+        })
+        .on('form:submit', function() {
+          console.log("School ID:", selectedSchoolId);
+          return false; // Don't submit form for this demo
+        });
       });
-    });
+
+      function isSelected() {
+        var term = $('#name').val();
+        var options = document.getElementById('school-list').children;
+        for(var i=0; i<options.length; i++) {
+          if (options[i].value === term) {
+            selectedSchoolId = options[i].dataset.id;
+            $('#school-list').empty();
+            return true;
+          }
+        }
+        selectedSchoolId = "";
+        return false;
+      }
+
+      function onInput() {
+        window.setTimeout(() => {
+          if (isSelected()) {
+            return;
+          }
+          
+          var term = $('#name').val();
+          if (term.length > 3) {
+            $.get(baseUrl + "?term=" + term, function(data) {
+              $('#school-list').empty();
+              var items = data.items;
+              var lenLimit = 20;
+              var length = (items.length > lenLimit) ? lenLimit : items.length;
+              for (var i=0; i<length; i++) {
+                var optionNode = document.createElement("option");
+                optionNode.value = items[i].text;
+                optionNode.dataset.id = items[i].id;
+                $('#school-list').append(optionNode);
+              }
+            });
+          }
+        }, 1);
+      }
     </script>
 
   </body>
